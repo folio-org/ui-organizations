37d0284707a7b09de26360ab0f085e4b756ed5a9
Mikita Siadykh <mikita_siadykh@epam.com>
2019-04-30 11:26:04 +0300

    UIORGS-50 Modal Select Contact with SearchAndSort implementation

--- src/common/resources/contacts.js
+++ src/common/resources/contacts.js
@@ -1,10 +1,15 @@
 import { CONTACTS_API } from '../constants';
 
-// eslint-disable-next-line import/prefer-default-export
 export const contactResource = {
   type: 'okapi',
   path: (queryParams, pathComponents) => {
     if (pathComponents.id) return `${CONTACTS_API}/${pathComponents.id}`;
     return undefined;
   },
+};
+
+export const baseContactsResource = {
+  type: 'okapi',
+  records: 'contacts',
+  path: CONTACTS_API,
 };
