3b7b59b110b413335c191df16597b0bfbec3a8ab
Aliaksei Chumakou <aliaksei_chumakou@epam.com>
2019-05-03 17:29:14 +0300

    UIORGS-48 remove falsy error notification (stripes-connect bug)

--- src/common/resources/contacts.js
+++ src/common/resources/contacts.js
@@ -1,6 +1,7 @@
 import { CONTACTS_API } from '../constants';
 
 export const contactResource = {
+  throwErrors: false,
   type: 'okapi',
   path: (queryParams, pathComponents) => {
     if (pathComponents.id) return `${CONTACTS_API}/${pathComponents.id}`;
@@ -9,7 +10,14 @@ export const contactResource = {
 };
 
 export const baseContactsResource = {
+  throwErrors: false,
   type: 'okapi',
   records: 'contacts',
   path: CONTACTS_API,
+};
+
+export const contactDetailsResource = {
+  throwErrors: false,
+  type: 'okapi',
+  path: `${CONTACTS_API}/:{id}`,
 };
