99f015ed40a2fed8ea25ddd64e9928864c6422cf
Yury Aniskou <klaus24128@gmail.com>
2019-07-09 12:24:36 +0300

    UIORGS-9 refactoring

--- src/Summary/SummaryView.js
+++ src/Summary/SummaryView.js
@@ -15,15 +15,14 @@ class SummaryView extends React.Component {
 
   render() {
     const { initialValues } = this.props;
-    const dataVal = initialValues || [];
     const columnWidths = { 'value': '50%', 'description': '50%' };
     const columnMapping = {
       'value': <FormattedMessage id="ui-organizations.summary.alias" />,
       'description': <FormattedMessage id="ui-organizations.summary.description" />,
     };
-    const getLanguage = languageLookUp(get(dataVal, 'language', ''));
+    const getLanguage = languageLookUp(get(initialValues, 'language', ''));
 
-    const metadata = get(dataVal, 'metadata');
+    const metadata = get(initialValues, 'metadata');
 
     return (
       <Row>
@@ -33,25 +32,25 @@ class SummaryView extends React.Component {
         <Col xs={4}>
           <KeyValue
             label={<FormattedMessage id="ui-organizations.summary.name" />}
-            value={get(dataVal, 'name', '')}
+            value={get(initialValues, 'name', '')}
           />
         </Col>
         <Col xs={4}>
           <KeyValue
             label={<FormattedMessage id="ui-organizations.summary.code" />}
-            value={get(dataVal, 'code', '')}
+            value={get(initialValues, 'code', '')}
           />
         </Col>
         <Col xs={4}>
           <KeyValue
             label={<FormattedMessage id="ui-organizations.summary.accountingCode" />}
-            value={get(dataVal, ['erpCode'], '')}
+            value={get(initialValues, ['erpCode'], '')}
           />
         </Col>
         <Col xs={4}>
           <KeyValue
             label={<FormattedMessage id="ui-organizations.summary.organizationStatus" />}
-            value={get(dataVal, 'status', '')}
+            value={get(initialValues, 'status', '')}
           />
         </Col>
         <Col xs={4}>
@@ -60,29 +59,30 @@ class SummaryView extends React.Component {
             value={getLanguage}
           />
         </Col>
-        {PrintBoolToCheckbox('ui-organizations.summary.isVendor', dataVal.isVendor, 4)}
+        {PrintBoolToCheckbox('ui-organizations.summary.isVendor', initialValues.isVendor, 4)}
         <Col xs={12}>
           <KeyValue
             label={<FormattedMessage id="ui-organizations.summary.description" />}
-            value={get(dataVal, 'description', '')}
+            value={get(initialValues, 'description', '')}
           />
         </Col>
         <Col xs={12} className={css.rowHeader}>
           <div className={css.subHeadings}>
-            <b>
-              <FormattedMessage id="ui-organizations.summary.alternativeNames" />
-            </b>
+            <b><FormattedMessage id="ui-organizations.summary.alternativeNames" /></b>
           </div>
           <MultiColumnList
             contentData={initialValues.aliases}
             columnWidths={columnWidths}
             columnMapping={columnMapping}
           />
-          <br />
         </Col>
       </Row>
     );
   }
 }
+
+SummaryView.defaultProps = {
+  initialValues: {},
+};
 
 export default SummaryView;
