8389b0b7db4200919a3a710e9aac2b30a5644e3a
Aliaksei Chumakou <aliaksei_chumakou@epam.com>
2019-10-03 17:20:33 +0300

    fix linter issues

--- test/bigtest/network/configs/categories.js
+++ test/bigtest/network/configs/categories.js
@@ -1,9 +1,11 @@
+import {
+  createGetAll,
+} from '@folio/stripes-acq-components/test/bigtest/network/configs';
+
 import { CATEGORIES_API } from '../../../../src/common/constants';
 
 const configCategories = server => {
-  server.get(CATEGORIES_API, (schema) => {
-    return schema.categories.all();
-  });
+  server.get(CATEGORIES_API, createGetAll('categories'));
 };
 
 export default configCategories;
--- test/bigtest/network/configs/contacts.js
+++ test/bigtest/network/configs/contacts.js
@@ -1,18 +1,19 @@
-import { noop } from 'lodash';
+import {
+  createGetAll,
+  createGetById,
+  createPost,
+  createPut,
+} from '@folio/stripes-acq-components/test/bigtest/network/configs';
+
 import { CONTACTS_API } from '../../../../src/common/constants';
 
+const SCHEMA_NAME = 'contacts';
+
 const configContacts = server => {
-  server.get(CONTACTS_API, (schema) => {
-    return schema.contacts.all();
-  });
-
-  server.get(`${CONTACTS_API}/:id`, (schema, request) => {
-    return schema.contacts.find(request.params.id).attrs;
-  });
-
-  server.put(`${CONTACTS_API}/:id`, noop);
-
-  server.post(`${CONTACTS_API}`, noop);
+  server.get(CONTACTS_API, createGetAll(SCHEMA_NAME));
+  server.put(`${CONTACTS_API}/:id`, createPut(SCHEMA_NAME));
+  server.delete(`${CONTACTS_API}/:id`, createGetById(SCHEMA_NAME));
+  server.post(`${CONTACTS_API}`, createPost(SCHEMA_NAME));
 };
 
 export default configContacts;
