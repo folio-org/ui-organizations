dda2df6c47bc0574d8de469100e8f00b89347bf5
Mikita Siadykh <mikita_siadykh@epam.com>
2019-12-11 15:24:19 +0300

    UIORGS-124 use buttons instead of checkboxes to select primary info (#173)

---
+++ src/common/components/ButtonIsPrimary/ButtonIsPrimary.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import PropTypes from 'prop-types';
+
+import { Button } from '@folio/stripes/components';
+
+import css from './ButtonIsPrimary.css';
+
+const ButtonIsPrimary = ({ dispatchChange, fields, fieldIndex, labelId }) => {
+  const isPrimary = fields.get(fieldIndex).isPrimary;
+  const changeIsPrimary = () => {
+    if (isPrimary) return;
+
+    fields.forEach((fieldName, i) => {
+      if (
+        (i !== fieldIndex && fields.get(i).isPrimary)
+        || i === fieldIndex
+      ) {
+        dispatchChange(`${fieldName}.isPrimary`, !fields.get(i).isPrimary);
+      }
+    });
+  };
+
+  return (
+    <Button
+      onClick={changeIsPrimary}
+      buttonStyle={isPrimary ? 'primary' : 'default'}
+      buttonClass={css.buttonIsPrimary}
+    >
+      <FormattedMessage id={labelId} />
+    </Button>
+  );
+};
+
+ButtonIsPrimary.propTypes = {
+  dispatchChange: PropTypes.func.isRequired,
+  fieldIndex: PropTypes.number.isRequired,
+  fields: PropTypes.object.isRequired,
+  labelId: PropTypes.string,
+};
+
+export default ButtonIsPrimary;
