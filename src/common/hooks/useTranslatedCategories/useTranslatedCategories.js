80314be4e166f194f39a70d79b75dda63a81a5e7
Andrei Shumski <Andrei_Shumski2@epam.com>
2020-12-14 15:01:31 +0300

    code review

--- src/common/hooks/useTranslatedCategories/useTranslatedCategories.js
+++ src/common/hooks/useTranslatedCategories/useTranslatedCategories.js
@@ -1,24 +1,27 @@
+import { useMemo } from 'react';
 import { useIntl } from 'react-intl';
 
 import { VENDOR_DEFAULT_CATEGORIES } from '../../constants';
 
-const useTranslatedCategories = (categories = []) => {
+const useTranslatedCategories = (categories) => {
   const intl = useIntl();
 
-  const translatedCategories = categories.map(category => {
-    const translationKey = VENDOR_DEFAULT_CATEGORIES[category.value];
-    const categoryValue = translationKey
-      ? intl.formatMessage({
-        id: `ui-organizations.contactInfo.vendorCategory.${translationKey}`,
-        defaultMessage: category.value,
-      })
-      : category.value;
+  const translatedCategories = useMemo(() => (
+    categories?.map(category => {
+      const translationKey = VENDOR_DEFAULT_CATEGORIES[category.value];
+      const categoryValue = translationKey
+        ? intl.formatMessage({
+          id: `ui-organizations.contactInfo.vendorCategory.${translationKey}`,
+          defaultMessage: category.value,
+        })
+        : category.value;
 
-    return ({
-      ...category,
-      value: categoryValue,
-    });
-  });
+      return ({
+        ...category,
+        value: categoryValue,
+      });
+    })),
+  [categories, intl]);
 
   return [translatedCategories];
 };
