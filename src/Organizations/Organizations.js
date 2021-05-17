1fd5e22c25e9c8ef0b060ac991ef5e990771da5b
AndreiShumski2 <Andrei_Shumski2@epam.com>
2021-05-17 14:13:20 +0300

    UIORGS-246 Implement Keyboard shortcuts modal (#382)

--- src/Organizations/Organizations.js
+++ src/Organizations/Organizations.js
@@ -3,13 +3,22 @@ import {
   Switch,
   Route,
 } from 'react-router-dom';
+import { FormattedMessage } from 'react-intl';
 
+import { AppContextMenu } from '@folio/stripes/core';
 import {
   checkScope,
   CommandList,
   defaultKeyboardShortcuts,
   HasCommand,
+  NavList,
+  NavListItem,
+  NavListSection,
 } from '@folio/stripes/components';
+import {
+  AcqKeyboardShortcutsModal,
+  useModalToggle,
+} from '@folio/stripes-acq-components';
 
 import {
   NOTES_ROUTE,
@@ -21,6 +30,7 @@ import { OrganizationCreate } from './OrganizationCreate';
 import { OrganizationEdit } from './OrganizationEdit';
 
 const Organizations = () => {
+  const [isOpen, toggleModal] = useModalToggle();
   const focusSearchField = () => {
     const el = document.getElementById('input-record-search');
 
@@ -34,38 +44,67 @@ const Organizations = () => {
       name: 'search',
       handler: focusSearchField,
     },
+    {
+      name: 'openShortcutModal',
+      shortcut: 'mod+alt+k',
+      handler: toggleModal,
+    },
   ];
 
   return (
-    <CommandList commands={defaultKeyboardShortcuts}>
-      <HasCommand
-        commands={shortcuts}
-        isWithinScope={checkScope}
-        scope={document.body}
-      >
-        <Switch>
-          <Route
-            path={NOTES_ROUTE}
-            component={Notes}
-          />
+    <>
+      <CommandList commands={defaultKeyboardShortcuts}>
+        <HasCommand
+          commands={shortcuts}
+          isWithinScope={checkScope}
+          scope={document.body}
+        >
+          <AppContextMenu>
+            {handleToggle => (
+              <NavList>
+                <NavListSection>
+                  <NavListItem
+                    id="keyboard-shortcuts-item"
+                    onClick={() => {
+                      handleToggle();
+                      toggleModal();
+                    }}
+                  >
+                    <FormattedMessage id="stripes-acq-components.appMenu.keyboardShortcuts" />
+                  </NavListItem>
+                </NavListSection>
+              </NavList>
+            )}
+          </AppContextMenu>
+          <Switch>
+            <Route
+              path={NOTES_ROUTE}
+              component={Notes}
+            />
 
-          <Route
-            path="/organizations/create"
-            component={OrganizationCreate}
-          />
+            <Route
+              path="/organizations/create"
+              component={OrganizationCreate}
+            />
 
-          <Route
-            path="/organizations/:id/edit"
-            component={OrganizationEdit}
-          />
+            <Route
+              path="/organizations/:id/edit"
+              component={OrganizationEdit}
+            />
 
-          <Route
-            path={ORGANIZATIONS_ROUTE}
-            component={OrganizationsListContainer}
-          />
-        </Switch>
-      </HasCommand>
-    </CommandList>
+            <Route
+              path={ORGANIZATIONS_ROUTE}
+              component={OrganizationsListContainer}
+            />
+          </Switch>
+        </HasCommand>
+      </CommandList>
+      {isOpen && (
+        <AcqKeyboardShortcutsModal
+          onClose={toggleModal}
+        />
+      )}
+    </>
   );
 };
 
