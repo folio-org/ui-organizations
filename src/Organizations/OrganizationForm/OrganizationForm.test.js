0cc2d21040fa21fd6552125d2bdabd8c9ab3eb7d
Mikita Siadykh <mikita_siadykh@epam.com>
2021-06-14 13:54:56 +0400

    test

--- src/Organizations/OrganizationForm/OrganizationForm.test.js
+++ src/Organizations/OrganizationForm/OrganizationForm.test.js
@@ -3,9 +3,26 @@ import { render, screen } from '@testing-library/react';
 import { queryHelpers } from '@testing-library/dom';
 import user from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
+import { useHistory } from 'react-router';
 
+import {
+  HasCommand,
+  expandAllSections,
+  collapseAllSections,
+} from '@folio/stripes/components';
+
+import { ORGANIZATIONS_ROUTE } from '../../common/constants';
 import OrganizationForm from './OrganizationForm';
 
+jest.mock('react-router', () => ({
+  ...jest.requireActual('react-router'),
+  useHistory: jest.fn(),
+}));
+jest.mock('@folio/stripes-components/lib/Commander', () => ({
+  HasCommand: jest.fn(({ children }) => <div>{children}</div>),
+  expandAllSections: jest.fn(),
+  collapseAllSections: jest.fn(),
+}));
 jest.mock(
   './OrganizationSummaryForm',
   () => ({ OrganizationSummaryForm: () => 'OrganizationSummaryForm' }),
@@ -126,6 +143,56 @@ describe('OrganizationForm', () => {
           .filter(collapseButton => collapseButton.getAttribute('aria-expanded') === 'true')
           .length,
       ).toBe(sections.length);
+    });
+  });
+
+  describe('Shortcuts', () => {
+    beforeEach(() => {
+      HasCommand.mockClear();
+      expandAllSections.mockClear();
+      collapseAllSections.mockClear();
+    });
+
+    it('should call expandAllSections when expandAllSections shortcut is called', () => {
+      renderViewContact();
+
+      HasCommand.mock.calls[0][0].commands.find(c => c.name === 'expandAllSections').handler();
+
+      expect(expandAllSections).toHaveBeenCalled();
+    });
+
+    it('should call collapseAllSections when collapseAllSections shortcut is called', () => {
+      renderOrganizationForm();
+
+      HasCommand.mock.calls[0][0].commands.find(c => c.name === 'collapseAllSections').handler();
+
+      expect(collapseAllSections).toHaveBeenCalled();
+    });
+
+    it('should navigate to edit view when edit shortcut is called', () => {
+      const pushMock = jest.fn();
+
+      useHistory.mockClear().mockReturnValue({
+        push: pushMock,
+      });
+
+      renderOrganizationForm();
+      HasCommand.mock.calls[0][0].commands.find(c => c.name === 'edit').handler();
+
+      expect(pushMock).toHaveBeenCalledWith(defaultProps.editUrl);
+    });
+
+    it('should navigate to list view when search shortcut is called', () => {
+      const pushMock = jest.fn();
+
+      useHistory.mockClear().mockReturnValue({
+        push: pushMock,
+      });
+
+      renderOrganizationForm();
+      HasCommand.mock.calls[0][0].commands.find(c => c.name === 'search').handler();
+
+      expect(pushMock).toHaveBeenCalledWith(ORGANIZATIONS_ROUTE);
     });
   });
 });
