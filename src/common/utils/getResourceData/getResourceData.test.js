8de2d8ebc3bcbf7488c4b40d8f43967ae8159768
Mikita Siadykh <mikita_siadykh@epam.com>
2021-06-18 11:23:46 +0400

    FAT-40 unit tests (#398)

---
+++ src/common/utils/getResourceData/getResourceData.test.js
@@ -0,0 +1,71 @@
+import { organization } from '../../../../test/jest/fixtures';
+
+import {
+  getResourceDataList,
+  getResourceDataItem,
+} from './getResourceData';
+
+const resourceName = 'organizations';
+
+describe('getResourceData utils', () => {
+  describe('getResourceDataList', () => {
+    it('should return defaultValue when resource is failed', () => {
+      const defaultValue = [];
+      const resources = {
+        [resourceName]: {
+          failed: true,
+        },
+      };
+
+      expect(getResourceDataList(resources, resourceName, defaultValue)).toBe(defaultValue);
+    });
+
+    it('should return defaultValue when resource records are not defined', () => {
+      const defaultValue = [];
+      const resources = {
+        [resourceName]: {
+          failed: false,
+          records: undefined,
+        },
+      };
+
+      expect(getResourceDataList(resources, resourceName, defaultValue)).toBe(defaultValue);
+    });
+
+    it('should return records when not loading and defined', () => {
+      const records = [organization];
+      const resources = {
+        [resourceName]: {
+          failed: false,
+          records,
+        },
+      };
+
+      expect(getResourceDataList(resources, resourceName)).toBe(records);
+    });
+  });
+
+  describe('getResourceDataItem', () => {
+    it('should return defaultValue when records are empty', () => {
+      const resources = {
+        [resourceName]: {
+          failed: false,
+          records: [],
+        },
+      };
+
+      expect(getResourceDataItem(resources, resourceName, organization)).toBe(organization);
+    });
+
+    it('should return defaultValue when resource is not empty', () => {
+      const resources = {
+        [resourceName]: {
+          failed: false,
+          records: [organization],
+        },
+      };
+
+      expect(getResourceDataItem(resources, resourceName)).toBe(organization);
+    });
+  });
+});
