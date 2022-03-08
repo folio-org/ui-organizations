ccd6d8c59f584ee8a967a20a2dec2730c8eb27e4
elsenhans <elsenhans@users.noreply.github.com>
2022-03-08 17:07:38 +0100

    UIORGS-283-create-settings-types add more tests

--- src/Settings/TypeSettings.test.js
+++ src/Settings/TypeSettings.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 
 import TypeSettings from './TypeSettings';
 
@@ -17,27 +16,13 @@ jest.mock('@folio/stripes-smart-components/lib/ControlledVocab', () => jest.fn((
     <button
       data-testid="button-new"
       type="button"
-      onClick={() => {}}
+      onClick={() => {
+        preCreateHook();
+        listSuppressor();
+      }}
     >
       New
     </button>
-    <button
-      data-testid="button-cancel"
-      type="button"
-      onClick={() => {}}
-    >
-      Cancel
-    </button>
-    <button
-      data-testid="button-save"
-      type="button"
-      onClick={() => {}}
-    >
-      Save
-    </button>
-    <select
-      id="select-type-status"
-    />
   </>
 )));
 
@@ -62,16 +47,5 @@ describe('TypeSettings component', () => {
 
   it('should display new button', async () => {
     expect(screen.getByText('New')).toBeInTheDocument();
-  });
-
-  it('should display buttons and select box', async () => {
-    userEvent.click(screen.getByText('New'));
-
-    const cancelButton = screen.getByRole('button', { name: 'Cancel' });
-    const saveButton = screen.getByRole('button', { name: 'Save' });
-    expect(cancelButton).toBeInTheDocument();
-    expect(saveButton).toBeInTheDocument();
-
-    expect(document.querySelector('#select-type-status')).toBeInTheDocument();
   });
 });
