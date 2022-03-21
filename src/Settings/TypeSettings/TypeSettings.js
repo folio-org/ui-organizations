a4037d808d2115282cebf76557d9172bdc1e6c4f
elsenhans <48911833+elsenhans@users.noreply.github.com>
2022-03-21 08:16:52 +0100

    UIORGS-283: Create settings types (#471)
    * UIORGS-283-create-settings-types add types to settings
    
    * UIORGS-283-create-settings-types add ticket to CHANGELOG
    
    * UIORGS-283-create-settings-types add test
    
    * UIORGS-283-create-settings-types adapt route and columns
    
    * UIORGS-283-create-settings-types add select for status
    
    * UIORGS-283-create-settings-types add path and permissions
    
    * UIORGS-283-create-settings-types update snapshots
    
    * UIORGS-283-create-settings-types update snapshots
    
    * UIORGS-283-create-settings-types remove permissions for organizations view
    
    * Lokalise: updates
    
    * Release v3.1.0 (#469)
    
    * bump up stripes-acq-version (#470)
    
    * UIORGS-283-create-settings-types update test snapshot
    
    * UIORGS-283-create-settings-types adapt interface version and CHANGELOG
    
    * UIORGS-283-create-settings-types adapt CHANGELOG
    
    * UIORGS-283-create-settings-types reset interface version
    
    * UIORGS-283-create-settings-types add translation settings.action
    
    * UIORGS-283-create-settings-types adapt translations for type settings
    
    * UIORGS-283-create-settings-types adapt columnMapping
    
    * UIORGS-283-create-settings-types add more tests
    
    * UIORGS-283-create-settings-types add more tests
    
    * UIORGS-283-create-settings-types translate label, add folder, named export
    
    * UIORGS-283-create-settings-types add index.js
    
    * UIORGS-283-create-settings-types rename file, adapt exports
    
    * UIORGS-283-create-settings-types adapt export
    
    * UIORGS-283-create-settings-types fixing bug and remove blank line
    
    Co-authored-by: Viola Elsenhans <elsenhans@users.noreply.github.com>
    Co-authored-by: FOLIO Translations Bot <38661258+folio-translations@users.noreply.github.com>
    Co-authored-by: Mikita Siadykh <mikita_siadykh@epam.com>

---
+++ src/Settings/TypeSettings/TypeSettings.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {
+  injectIntl,
+  FormattedMessage,
+} from 'react-intl';
+import { Field } from 'redux-form';
+
+import { stripesShape } from '@folio/stripes/core';
+import { Select } from '@folio/stripes/components';
+import { ControlledVocab } from '@folio/stripes/smart-components';
+import { typeStatus } from './constants';
+
+class TypeSettings extends Component {
+  constructor(props) {
+    super(props);
+    this.connectedControlledVocab = props.stripes.connect(ControlledVocab);
+  }
+
+  getDataOptions(field) {
+    return field.map((item) => ({
+      label: this.props.intl.formatMessage({ id: `ui-organizations.settings.typeStatus.${item.value}` }),
+      value: item.value,
+    }));
+  }
+
+  render() {
+    const { stripes } = this.props;
+    const columnMapping = {
+      name: <FormattedMessage id="ui-organizations.settings.type.name" />,
+      status: <FormattedMessage id="ui-organizations.settings.type.status" />,
+      action: <FormattedMessage id="ui-organizations.settings.action" />,
+    };
+
+    const getDisableAttr = () => ({
+      disabled: !stripes.hasPerm('ui-organizations.settings'),
+    });
+
+    const actionProps = {
+      create: getDisableAttr,
+      edit: getDisableAttr,
+      delete: getDisableAttr,
+    };
+
+    const fieldComponents = {
+      'status': ({ fieldProps }) => (
+        <Field
+          component={Select}
+          dataOptions={this.getDataOptions(typeStatus)}
+          id="select-type-status"
+          placeholder=" "
+          {...fieldProps}
+        />
+      ),
+    };
+
+    const setRequiredValidation = (values) => {
+      const errors = {};
+
+      if (!values.status) {
+        errors.status = <FormattedMessage id="stripes-core.label.missingRequiredField" />;
+      }
+
+      return errors;
+    };
+
+    return (
+      <this.connectedControlledVocab
+        actionProps={actionProps}
+        baseUrl="organizations-storage/organization-types"
+        columnMapping={columnMapping}
+        fieldComponents={fieldComponents}
+        hiddenFields={['lastUpdated', 'numberOfObjects']}
+        id="types"
+        label={<FormattedMessage id="ui-organizations.settings.types" />}
+        labelSingular={<FormattedMessage id="ui-organizations.settings.type" />}
+        nameKey="types"
+        objectLabel={<FormattedMessage id="ui-organizations.settings.types" />}
+        records="organizationTypes"
+        sortby="name"
+        stripes={stripes}
+        validate={setRequiredValidation}
+        visibleFields={['name', 'status']}
+      />
+    );
+  }
+}
+
+TypeSettings.propTypes = {
+  stripes: stripesShape.isRequired,
+  intl: PropTypes.shape({
+    formatMessage: PropTypes.func.isRequired,
+  }),
+};
+
+export default injectIntl(TypeSettings);
---
+++ src/Settings/TypeSettings/index.js
@@ -0,0 +1,1 @@
+export { default as TypeSettings } from './TypeSettings';
