f65539a05b68c52fe11c9da5079358f2012a53c1
Yury Saukou <yury_saukou@epam.com>
2023-06-07 11:47:44 +0400

    UIORGS-373 Organizations filters consistency in 'ui-organizations' and 'ui-plugin-find-organization'

--- src/common/constants/organization.js
+++ src/common/constants/organization.js
@@ -1,16 +1,3 @@
-import React from 'react';
-import { FormattedMessage } from 'react-intl';
-
-export const ORGANIZATION_STATUS = {
-  active: 'Active',
-  inactive: 'Inactive',
-  pending: 'Pending',
-};
-
-export const ORGANIZATION_STATUS_LABELS = {
-  [ORGANIZATION_STATUS.active]: <FormattedMessage id="ui-organizations.organizationStatus.active" />,
-  [ORGANIZATION_STATUS.inactive]: <FormattedMessage id="ui-organizations.organizationStatus.inactive" />,
-  [ORGANIZATION_STATUS.pending]: <FormattedMessage id="ui-organizations.organizationStatus.pending" />,
-};
+export { ORGANIZATION_STATUS } from '@folio/plugin-find-organization';
 
 export const DICT_CATEGORIES = 'vendorCategories';
