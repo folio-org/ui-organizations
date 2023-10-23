dfa458b64e1650cdab2bf9ae752963db945b8f27
Alisher Musurmonov <alisher_musurmonov1@epam.com>
2023-10-23 13:24:03 +0500

    tests: add test coverages

--- src/Settings/BankingAccountTypeSettings/BankingAccountTypeSettings.js
+++ src/Settings/BankingAccountTypeSettings/BankingAccountTypeSettings.js
@@ -1,62 +1,72 @@
+import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
-import { stripesConnect, stripesShape } from '@folio/stripes/core';
+import { stripesShape } from '@folio/stripes/core';
 import { ControlledVocab } from '@folio/stripes/smart-components';
 import { getControlledVocabTranslations } from '@folio/stripes-acq-components';
 
 import { BANKING_ACCOUNT_TYPES_API } from '../constants';
 
-const BankingAccountTypeSettings = ({ stripes }) => {
-  const ConnectedComponent = stripes.connect(ControlledVocab);
+class BankingAccountTypeSettings extends React.Component {
+  constructor(props) {
+    super(props);
+    this.connectedControlledVocab = props.stripes.connect(ControlledVocab);
+  }
 
-  const columnMapping = {
-    value: <FormattedMessage id="ui-organizations.settings.name" />,
-    action: <FormattedMessage id="ui-organizations.settings.action" />,
-  };
+  render() {
+    const { stripes } = this.props;
 
-  const hasEditPerms = stripes.hasPerm('ui-organizations.settings');
-  const actionSuppressor = {
-    edit: () => !hasEditPerms,
-    delete: () => !hasEditPerms,
-  };
+    const columnMapping = {
+      value: <FormattedMessage id="ui-organizations.settings.name" />,
+      action: <FormattedMessage id="ui-organizations.settings.action" />,
+    };
 
-  const setUniqValidation = (value, index, items) => {
-    const errors = {};
+    const hasEditPerms = stripes.hasPerm('ui-organizations.settings');
+    const actionSuppressor = {
+      edit: () => !hasEditPerms,
+      delete: () => !hasEditPerms,
+    };
 
-    const isBankingAccountTypeExist = items.some(({ id, name }) => {
-      return name?.toLowerCase() === value?.name?.toLowerCase() && id !== value?.id;
-    });
+    const setUniqValidation = (value, index, items) => {
+      const errors = {};
 
-    if (isBankingAccountTypeExist) {
-      errors.name = <FormattedMessage id="ui-organizations.settings.accountTypes.save.error.accountTypeMustBeUnique" />;
-    }
+      const isBankingAccountTypeExist = items.some(({ id, name }) => {
+        return name?.toLowerCase() === value?.name?.toLowerCase() && id !== value?.id;
+      });
 
-    return errors;
-  };
+      if (isBankingAccountTypeExist) {
+        errors.name = <FormattedMessage id="ui-organizations.settings.accountTypes.save.error.accountTypeMustBeUnique" />;
+      }
 
-  return (
-    <ConnectedComponent
-      actionSuppressor={actionSuppressor}
-      canCreate={hasEditPerms}
-      stripes={stripes}
-      baseUrl={BANKING_ACCOUNT_TYPES_API}
-      records="bankingAccountTypes"
-      label={<FormattedMessage id="ui-organizations.settings.bankingAccountTypes" />}
-      translations={getControlledVocabTranslations('ui-organizations.settings.bankingAccountTypes')}
-      objectLabel="BankingAccountTypes"
-      visibleFields={['name']}
-      columnMapping={columnMapping}
-      hiddenFields={['lastUpdated', 'numberOfObjects']}
-      nameKey="bankingAccountTypes"
-      id="bankingAccountTypes"
-      validate={setUniqValidation}
-      sortby="name"
-    />
-  );
-};
+      return errors;
+    };
+
+    const ConnectedComponent = this.connectedControlledVocab;
+
+    return (
+      <ConnectedComponent
+        actionSuppressor={actionSuppressor}
+        canCreate={hasEditPerms}
+        stripes={stripes}
+        baseUrl={BANKING_ACCOUNT_TYPES_API}
+        records="bankingAccountTypes"
+        label={<FormattedMessage id="ui-organizations.settings.bankingAccountTypes" />}
+        translations={getControlledVocabTranslations('ui-organizations.settings.bankingAccountTypes')}
+        objectLabel="BankingAccountTypes"
+        visibleFields={['name']}
+        columnMapping={columnMapping}
+        hiddenFields={['lastUpdated', 'numberOfObjects']}
+        nameKey="bankingAccountTypes"
+        id="bankingAccountTypes"
+        validate={setUniqValidation}
+        sortby="name"
+      />
+    );
+  }
+}
 
 BankingAccountTypeSettings.propTypes = {
   stripes: stripesShape.isRequired,
 };
 
-export default stripesConnect(BankingAccountTypeSettings);
+export default BankingAccountTypeSettings;
---
+++ src/Settings/BankingAccountTypeSettings/BankingAccountTypeSettings.test.js
@@ -0,0 +1,59 @@
+import { render, screen } from '@folio/jest-config-stripes/testing-library/react';
+
+import { ControlledVocab } from '@folio/stripes/smart-components';
+
+import BankingAccountTypeSettings from './BankingAccountTypeSettings';
+
+jest.mock('@folio/stripes/core', () => ({
+  ...jest.requireActual('@folio/stripes/core'),
+  useStripes: jest.fn(),
+}));
+
+jest.mock('@folio/stripes-smart-components/lib/ControlledVocab', () => jest.fn(({
+  rowFilter,
+  label,
+  rowFilterFunction,
+  preCreateHook,
+  listSuppressor,
+}) => (
+  <>
+    {label}
+    <div onChange={rowFilterFunction}>{rowFilter}</div>
+    <button
+      data-testid="button-new"
+      type="button"
+      onClick={() => {
+        preCreateHook();
+        listSuppressor();
+      }}
+    >
+      New
+    </button>
+  </>
+)));
+
+const stripesMock = {
+  connect: component => component,
+  hasPerm: jest.fn(() => true),
+  clone: jest.fn(),
+};
+
+const renderCategorySettings = () => render(<BankingAccountTypeSettings stripes={stripesMock} resources={[]} />);
+
+describe('BankingAccountTypeSettings', () => {
+  it('should render component', () => {
+    renderCategorySettings();
+
+    expect(screen.getByText('New'));
+    expect(screen.getByText('ui-organizations.settings.bankingAccountTypes'));
+  });
+
+  it('should check action suppression', () => {
+    renderCategorySettings();
+
+    const { actionSuppressor } = ControlledVocab.mock.calls[0][0];
+
+    expect(actionSuppressor.edit()).toBeFalsy();
+    expect(actionSuppressor.delete()).toBeFalsy();
+  });
+});
