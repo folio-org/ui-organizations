f2fe2f09dbef425772b9a0fab1750b7491ec62c5
Yury Saukou <yury_saukou@epam.com>
2024-01-04 12:53:02 +0400

    UIORGS-397 Update the correct field in the organization when creating a new privileged contact (#598)

--- src/contacts/EditContact/util.js
+++ src/contacts/EditContact/util.js
@@ -1,15 +1,17 @@
-// eslint-disable-next-line import/prefer-default-export
+import get from 'lodash/get';
+
 export const saveContact = (mutator, contact, org, isPrivilegedContactUrl = false) => {
   const isNew = !contact.id;
   const currentMutator = isPrivilegedContactUrl ? mutator.privilegedContact : mutator.contact;
   const httpMethod = isNew ? currentMutator.POST : currentMutator.PUT;
+  const fieldName = isPrivilegedContactUrl ? 'privilegedContacts' : 'contacts';
 
   return httpMethod(contact)
     .then(savedContact => {
       if (isNew && org && org.id) {
         mutator.contactsOrg.PUT({
           ...org,
-          contacts: [...org.contacts, savedContact.id],
+          [fieldName]: [...get(org, fieldName, []), savedContact.id],
         });
       }
 
